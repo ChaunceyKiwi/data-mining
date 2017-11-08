92f699e9de23c718e485282f298036e2d2f88f59
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-11-07 20:45:31 -0800

    Update programming assignment3

--- coding/pa3.R
+++ coding/pa3.R
@@ -1,31 +1,23 @@
 library(tree)
+library(cluster)
+library(caret)
+library(ROCR)
+library(Metrics)
 library(randomForest)
 setwd("/Users/Chauncey/Workspace/data-mining/coding") # set working directory
 
 ############################ Data preprocessing ########################################################
-
-# Read in the dataset and split the dataset randomly into 80% training data and 20% test data
 set.seed(1) # set the seed of sample to 1
 titantic = read.csv("titanic3.csv") # read in the Titantic dataset from .csv file
-
-# split the dataset randomly into 80% training data and 20% test data
-traing_data_size = floor(0.8 * nrow(titantic))
+traing_data_size = floor(0.8 * nrow(titantic)) # split the dataset into training data and test data
 training_data_index = sample(seq_len(nrow(titantic)), size = traing_data_size) 
 training_data = titantic[training_data_index, ]
 test_data = titantic[-training_data_index, ]
-
-# Report the number of missing values per attribute in the training and test dataset
 training_data[training_data == ""] = NA # set all empty values in training set as NA
 test_data[test_data == ""] = NA # set all empty value in test set as NA
-
-# Drop name, ticket, boat, body
-drop <- c("boat", "body", "name", "ticket", "cabin", "home.dest")
+drop <- c("boat", "body", "name", "ticket", "cabin", "home.dest") # Drop name, ticket, boat, body
 training_data = training_data[,!(names(training_data) %in% drop)]
 test_data = test_data[,!(names(test_data) %in% drop)]
-
-# 1. For attributes age, replace the missing value with the meaning value
-# 2. Attribute fare and embarked has very few missing values, delete the
-#    records that have missing values in these attributes. 
 training_data$age[is.na(training_data$age)] = mean(training_data$age, na.rm = TRUE)
 test_data$age[is.na(test_data$age)] = mean(test_data$age, na.rm = TRUE)
 training_data = training_data[!is.na(training_data$fare),]
@@ -37,46 +29,54 @@ test_data = test_data[!is.na(test_data$embarked),]
 training_data$survived = factor(training_data$survived)
 test_data$survived = factor(test_data$survived)
 
-#########################################################################################################
+############################### Task of Assignment 3 ######################################################
 
-# Task 1
-decision_tree <- tree(survived~., data = training_data)
-print(summary(decision_tree))
+# Task 1, the size of tree is 13
+# Task 2, the most important five attributes: sex, pclass, fare, age, sibsp
+decision_tree = tree(survived~., data = training_data)
+# print(decision_tree)
+# print(summary(decision_tree))
 plot(decision_tree); text(decision_tree)
 
-# Task 2  most important five attribute: sex, pclass, fare, age, sibsp
-
 # Task 3
-cv_decision_tree = cv.tree(decision_tree)
-plot(cv_decision_tree);
-best_size = cv_decision_tree$size[which(cv_decision_tree$dev == min(cv_decision_tree$dev))]
+cv_decision_tree = cv.tree(decision_tree, FUN = prune.misclass)
+plot(cv_decision_tree)
+best_size = min(cv_decision_tree$size[which(cv_decision_tree$dev == min(cv_decision_tree$dev))])
 decision_tree_pruned = prune.misclass(decision_tree, best = best_size)
 plot(decision_tree_pruned); text(decision_tree_pruned)
 
 # Task 4
-predict_res = predict(decision_tree_pruned, test_data, "class")
-confusion_matrix = confusionMatrix(data = predict_res, reference = test_data$survived) # 0.8206
+predict_res = predict(decision_tree_pruned, test_data, "vector")
+predict_res = predict_res[, 2]
+predict_res_bi = round(predict_res)
+confusion_matrix = confusionMatrix(data = predict_res_bi, reference = test_data$survived) # 0.8206
 
 # Convert factor back to numeric to enable prediction
-predict_res = as.numeric(as.character(predict_res))
+## predict_res = as.numeric(as.character(predict_res))
 actual_value = as.numeric(as.character(test_data$survived))
 pr = prediction(predict_res, actual_value)
 predit_preformance = performance(pr, measure = "tpr", x.measure = "fpr")
 plot(predit_preformance, main = "ROC Curve for pruned decision tree")
-print(auc(test_data$survived, predict_res))
+print(paste0("AUC: ", auc(test_data$survived, predict_res))) # AUC = 0.81289
 
 # Task 5
-random_forest = randomForest(survived~., data = training_data, nTree = 100)
-random_forest_predict = predict(random_forest, test_data, type = "class")
-print(mean(random_forest_predict == actual_value))
+random_forest = randomForest(survived~., data = training_data, nTree = 0)
+random_forest_predict = predict(random_forest, test_data, type = "prob")
+random_forest_predict = random_forest_predict[, 2]
+random_forest_predict_bi = round(random_forest_predict)
+print(paste0("Accuracy: ", mean(random_forest_predict_bi == actual_value))) # 0.8053435
+pr = prediction(random_forest_predict, actual_value)
+predit_preformance2 = performance(pr, measure = "tpr", x.measure = "fpr")
+plot(predit_preformance2, main = "ROC Curve for random_forest with size 100")
+print(paste0("AUC: ", auc(test_data$survived, random_forest_predict))) # AUC = 0.8934436
 
-# Task 6
-random_forest = randomForest(survived~., data = training_data, nTree = 100)
-random_forest_predict = predict(random_forest, test_data, type = "class")
-print(mean(random_forest_predict == actual_value))
+# # Task 6
+# random_forest = randomForest(survived~., data = training_data, nTree = 120)
+# random_forest_predict = predict(random_forest, test_data, type = "class")
+# print(mean(random_forest_predict == actual_value))
 
-# Task 7 
-# sex, fare, age, pclass, sibsp
-# attributes are the same compared with attributes selected in task2
-imp = importance(random_forest)
-imp_plot = varImpPlot(random_forest)
+# # Task 7 
+# # sex, fare, age, pclass, sibsp
+# # attributes are the same compared with attributes selected in task2
+# imp = importance(random_forest)
+# imp_plot = varImpPlot(random_forest)
